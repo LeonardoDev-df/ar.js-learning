63371825e3241caa149c28b182ab980a4c1cc166
Leonardo Lopes <69646199+LeonardoDev-df@users.noreply.github.com>
2023-08-31 15:06:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 AFRAME.registerComponent('markerhandler', {
     init: function () {
         this.marker1 = document.querySelector('#marker1');
+        this.marker2 = document.querySelector('#marker2');
   
         alert("Aproxime os marcadores da câmera");
     },
     tick: function () {
+        const marker1Visible = this.marker1.object3D.visible;
+        const marker2Visible = this.marker2.object3D.visible;
+        
         this.el.addEventListener('click', function (evt) {
             const soundEl = evt.target;
             soundEl.components.sound.playSound();
